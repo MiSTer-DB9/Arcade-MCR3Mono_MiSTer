7a0c1e2b8a5eb8c47acd7771c7a1415ad55ff585
Miguel-T80c <the80scomputers@gmail.com>
2020-02-23 01:06:02 +0100

    Change DB9MD OSD buttons

--- Arcade-MCR3Mono.sv
+++ Arcade-MCR3Mono.sv
@@ -256,7 +256,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.joystick_analog_2(joy3a),
 	.joystick_analog_3(joy4a),
 
-	.joy_raw(joydb9md_1[5:0]),
+	.joy_raw({joydb9md_1[4],joydb9md_1[6],joydb9md_1[3:0]}),
 .ps2_key(ps2_key)
 );
 
